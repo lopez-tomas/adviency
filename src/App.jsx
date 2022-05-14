c8db32c024a7bf892cb9a9a98ae29d8db106bdd4
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-14 19:42:59 -0300

    [FEAT]: showing gifts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.sass';
+
+const giftsList = [
+  'Mouse Logitech G Pro',
+  'Manga Gunnm: Battle Angel Alita #9',
+  'Star Wars cup'
+]
 
 const App = () => {
+  const [gifts, setGifts] = useState(giftsList);
+
   return (
-    <div className='App'>
-      <h1>Hello, World!</h1>
-    </div>
+    <main className='App'>
+      <h1>Gifts:</h1>
+      <ul>
+        {gifts.map((gift, index) => (
+          <li key={index}>{gift}</li>
+        ))}
+      </ul>
+    </main>
   )
 }
 
