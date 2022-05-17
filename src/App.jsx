e66bb10a152bfad8f8cc375e09832228681bf2fd
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-17 14:11:55 -0300

    [FEAT/styles]: some styles added to App.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import AppContext from './context/AppContext';
 import useInitialState from './hooks/useInitialState';
 import Form from './containers/Form';
 import GiftList from './containers/GiftList';
+import './styles/App.sass';
 
 const App = () => {
   const initialState = useInitialState();
 
   return (
     <AppContext.Provider value={initialState}>
-      <main className='App'>
+      <main className="App">
         <section className="Form-container">
           <h1>Gifts</h1>
           <Form />
