8b5046391b3c31038e36ebf0f0e73124fabcbf5e
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-18 23:03:13 -0300

    [FEAT]: using AppContext & useInitialState hook. Displaying containers in view.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import AppContext from './context/AppContext';
+import useInitialState from './hooks/useInitialState';
+import Form from './containers/Form';
+import GiftList from './containers/GiftList';
 
 const App = () => {
+  const initialState = useInitialState();
+
   return (
-    <div className='App'>
-      <h1>Hello, World!</h1>
-    </div>
+    <AppContext.Provider value={initialState}>
+      <main className="App">
+        <section className="Form-container">
+          <h1>Gifts</h1>
+          <Form />
+        </section>
+        <section className="GiftList-container">
+          <GiftList />
+        </section>
+      </main>
+    </AppContext.Provider>
   )
 }
 
