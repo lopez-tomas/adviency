dbdb8a090c3bb0e873a370c0731cc4cc1900d9b0
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-19 16:04:13 -0300

    [FEAT]: using useEffect & useState hooks to change areThereGifts status. If it's set to TRUE, List is displayed along with Revemo ALL gifts button. If it's set to FALSE, List is not displayed and an image and text is displayed instead.

--- src/containers/GiftList.jsx
+++ src/containers/GiftList.jsx
@@ -1,19 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import AppContext from '../context/AppContext';
 import Gift from '../components/Gift';
+import xmas from '../images/undraw-xmas-surprise.svg';
+import '../styles/GiftList.sass';
 
 const GiftList = () => {
   const { state, removeAll } = useContext(AppContext);
+  const [areThereGifts, setAreThereGifts] = useState(false);
+
+  useEffect(() => {
+    if (state.gifts.length !== 0) {
+      setAreThereGifts(true);
+    } else {
+      setAreThereGifts(false);
+    }
+  }, [state.gifts])
+
 
   return (
-    <div className="GiftList">
-      <ul className="List">
-        {state.gifts.map(gift => (
-          <Gift key={gift.id} gift={gift} />
-        ))}
-      </ul>
-      <button onClick={removeAll}>Remove ALL</button>
-    </div>
+    areThereGifts
+      ?
+        <div className="GiftList">
+          <ul className="List">
+            {state.gifts.map(gift => (
+              <Gift key={gift.id} gift={gift} />
+            ))}
+          </ul>
+          <div className="noGifts-container">
+            <button className="noGifts-button" onClick={removeAll}>Remove ALL</button>
+          </div>
+        </div>
+      :
+        <div className="GiftList">
+          <div className="noGifts-container">
+            <img className="noGifts-image" src={xmas} alt="No gifts:(" />
+            <h2>No gifts added :(</h2>
+          </div>
+        </div>
   )
 }
 
