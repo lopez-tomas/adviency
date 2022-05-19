c3bb4f56303d63fac3d8e23db7c415cf0b80986e
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-19 17:11:35 -0300

    [FEAT/containers]: Form & GiftList containers created.

---
+++ src/containers/Form.jsx
@@ -0,0 +1,45 @@
+import React, { useContext, useRef } from 'react';
+import AppContext from '../context/AppContext';
+
+const Form = () => {
+  const {state, addGift} = useContext(AppContext);
+  const form = useRef(null);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const formData = new FormData(form.current);
+    let gift = formData.get("gift");
+    formData.set("gift", "");
+
+    if (gift !== "") {
+      const data = {
+        id: state.lastId,
+        gift: gift
+      }
+
+      addGift(data);
+      form.current.reset();
+    }
+  }
+
+  return (
+    <form className="Form" ref={form}>
+      <input
+        autoFocus
+        className="input add-input"
+        type="text"
+        name="gift"
+        placeholder="Your gift"
+        required
+      />
+      <input
+        onClick={handleAdd}
+        className="btn add-btn"
+        type="submit"
+        value="Add gift"
+      />
+    </form>
+  )
+}
+
+export default Form;
---
+++ src/containers/GiftList.jsx
@@ -0,0 +1,41 @@
+import React, { useContext, useState, useEffect } from 'react';
+import AppContext from '../context/AppContext';
+import Gift from '../components/Gift';
+import xmas from '../images/undraw-xmas-surprise.svg';
+
+const GiftList = () => {
+  const {state, removeAll} = useContext(AppContext);
+  const [areThereGifts, setAreThereGifts] = useState(false);
+
+  useEffect(() => {
+    if (state.gifts.length !== 0) {
+      setAreThereGifts(true);
+    } else {
+      setAreThereGifts(false);
+    }
+  }, [state.gifts]);
+
+  return (
+    areThereGifts
+      ?
+        <div className="GiftList">
+          <ul className="List">
+            {state.gifts.map(gift => (
+              <Gift key={gift.id} gift={gift} />
+            ))}
+          </ul>
+          <div className="noGifts-container">
+            <button onClick={removeAll}>Delete ALL</button>
+          </div>
+        </div>
+      :
+        <div className="GiftList">
+          <div className="noGifts-container">
+            <img src={xmas} alt="No gifts:(" />
+            <h2>No gifts added :(</h2>
+          </div>
+        </div>
+  )
+}
+
+export default GiftList;
