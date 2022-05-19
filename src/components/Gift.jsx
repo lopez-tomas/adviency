48554123b801895b0242b2135c0ca65dbb9b644d
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-19 15:52:50 -0300

    minor changes.

--- src/components/Gift.jsx
+++ src/components/Gift.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
+import '../styles/Gift.sass';
 
 const Gift = ({ gift }) => {
   const { removeGift } = useContext(AppContext);
@@ -9,9 +10,9 @@ const Gift = ({ gift }) => {
   }
 
   return (
-    <li>
-      {gift.gift}
-      <button onClick={() => handleRemove(gift.id)}>X</button>
+    <li className="Gift">
+      <p>{gift.gift}</p>
+      <button className="btn remove-btn" onClick={() => handleRemove(gift.id)}>X</button>
     </li>
   )
 }
