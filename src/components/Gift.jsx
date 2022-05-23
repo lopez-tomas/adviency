855745e96796bca9388a58a3f7f31e2a418c7f0a
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-23 18:47:13 -0300

    [FEAT/components]: Gift component created.

---
+++ src/components/Gift.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import noImage from '../images/no-image.jpg';
+
+const Gift = ({ gift }) => {
+  const { removeGift } = useContext(AppContext);
+
+  const handleRemove = (id) => {
+    removeGift(id);
+  }
+
+  return (
+    <li className="Gift">
+      <img className="gift-img" src={gift.image || noImage} height="70" width="70" alt={`Gift: ${gift.gift}`} />
+      <p className="gift-name">{gift.gift}</p>
+      <p className="gift-quantity">x {gift.quantity}</p>
+      <button
+        onClick={() => handleRemove(gift.id)}
+        className="btn remove-btn"
+      >
+        X
+      </button>
+    </li>
+  )
+}
+
+export default Gift;
