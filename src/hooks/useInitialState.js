5167ba761443c0f996986dfc625a5b3bb3555786
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-24 10:44:35 -0300

    [FEAT]: AppContext & useInitialState hook created.

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+
+const initialState = {
+  gifts: JSON.parse(localStorage.getItem("gifts")) || [],
+  lastId: JSON.parse(localStorage.getItem("lastId")) || 1
+}
+
+const useInitialState = () => {
+  const [state, setState] = useState(initialState);
+
+  useEffect(() => {
+    localStorage.setItem("gifts", JSON.stringify(state.gifts));
+    localStorage.setItem("lastId", JSON.stringify(state.lastId));
+  }, [state.gifts, state.lastId])
+
+  const addGift = (payload) => {
+    setState({
+      ...state,
+      gifts: [...state.gifts, payload],
+      lastId: state.lastId + 1
+    })
+  }
+
+  const removeGift = (id) => {
+    setState({
+      ...state,
+      gifts: state.gifts.filter(gift => gift.id !== id)
+    })
+  }
+
+  const removeAll = () => {
+    setState({
+      gifts: [],
+      lastId: 1
+    })
+  }
+
+  return {
+    state,
+    addGift,
+    removeGift,
+    removeAll
+  }
+}
+
+export default useInitialState;
