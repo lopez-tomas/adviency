b60da8a2448e0576f4827bef3f1323001a55664c
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-30 16:49:24 -0300

    [FEAT/containers]: Modal container created.

---
+++ src/containers/Modal.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import Form from './Form';
+
+const Modal = ({ show, onClose, idGift }) => {
+  const closeOnEscapeKeyDown = (e) => {
+    if ((e.charCode || e.keyCode) === 27) {
+      onClose();
+    }
+  }
+
+  useEffect(() => {
+    document.body.addEventListener("keydown", closeOnEscapeKeyDown);
+
+    return function cleanup() {
+      document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
+    }
+  })
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="Modal" onClick={onClose}>
+      <div className="Modal-content" onClick={e => e.stopPropagation()}>
+        <main className="Modal-body">
+          <Form onClose={onClose} idGift={idGift} />
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default Modal;
