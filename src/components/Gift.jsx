7debb0674098d565fc82758e1a1aff7eb8727435
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-30 15:41:07 -0300

    [FEAT/component]: Gift component created.

---
+++ src/components/Gift.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from 'react';
+import AppContext from '../context/AppContext';
+import Modal from '../containers/Modal';
+import '../styles/Gift.sass';
+
+const noImage = "https://raw.githubusercontent.com/lopez-tomas/adviency/day10/src/images/no-image.jpg";
+
+const Gift = ({ gift }) => {
+  const { removeGift } = useContext(AppContext);
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <li className="Gift">
+        <img className="gift-img" src={gift.image ? gift.image : noImage} alt={gift.gift} />
+        <p className="gift-name">
+          {gift.gift}
+          <span className="gift-quantity"> ({gift.quantity})</span><br />
+          <span className="gift-to">{gift.to}</span>
+        </p>
+
+        <div>
+          <button
+            onClick={() => setShow(!show)}
+            className="btn edit-btn"
+          >
+            E
+          </button>
+          <button
+            onClick={() => removeGift(gift.id)}
+            className="btn remove-btn"
+          >
+            X
+          </button>
+        </div>
+      </li>
+
+      <Modal show={show} onClose={() => setShow(false)} idGift={gift.id} />
+    </>
+  )
+}
+
+export default Gift;
