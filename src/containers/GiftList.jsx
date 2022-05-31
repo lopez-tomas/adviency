8eef68fe5e9359560cd9aeb56dd8a7f8499ba75a
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-05-30 21:26:33 -0300

    [FEAT]: Adviency - Day 18 finished. Total price displayed.

--- src/containers/GiftList.jsx
+++ src/containers/GiftList.jsx
@@ -4,6 +4,10 @@ import Gift from '../components/Gift';
 import xmas from '../images/undraw-xmas-surprise.svg';
 import '../styles/GiftList.sass';
 
+const totalPriceReducer = (prev, curr) => {
+  return prev + (parseFloat(curr.price) * parseInt(curr.quantity));
+}
+
 const GiftList = () => {
   const { state, removeAll } = useContext(AppContext);
   const [areThereGifts, setAreThereGifts] = useState(false);
@@ -21,6 +25,9 @@ const GiftList = () => {
               <Gift key={gift.id} gift={gift} />
             ))}
           </ul>
+          <div className="total-container">
+            <p>Total: $ {(state.gifts.reduce(totalPriceReducer, 0)).toFixed(2)}</p>
+          </div>
           <div className="noGifts-container">
             <button onClick={removeAll} className="noGifts-btn">Remove ALL</button>
           </div>
