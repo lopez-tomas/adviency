d08c6edccc6dab1e242836fc15ae508cdcb0d879
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-05 02:21:51 -0300

    [FEAT]: Adviency - Day 20 finished.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppContext from './context/AppContext';
+import useInitialState from './hooks/useInitialState';
+import Modal from './containers/Modal';
+import Form from './containers/Form';
+import GiftList from './containers/GiftList';
+import './styles/App.sass';
 
-const App = () => {
+const App: React.FC = () => {
+  const initialState = useInitialState();
+  const [show, setShow] = useState(false);
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <AppContext.Provider value={initialState}>
+      <main className='App'>
+        <section className='Form-container'>
+          <h1>Gifts</h1>
+          <button onClick={() => setShow(!show)} className='btn add-gift'>Add gift</button>
+
+          <Modal show={show} onClose={() => setShow(false)}>
+            <Form onClose={() => setShow(false)} />
+          </Modal>
+        </section>
+        <section className='GiftList-container'>
+          <GiftList />
+        </section>
+      </main>
+    </AppContext.Provider>
   )
 }
 
