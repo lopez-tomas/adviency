9b54048affe85e7802cf6d115f69e815e493c80b
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-04 22:02:18 -0300

    [FEAT]: total gifts price displayed in GiftList.

--- src/containers/GiftList.tsx
+++ src/containers/GiftList.tsx
@@ -1,9 +1,15 @@
 import { ContextProps } from '../types';
+import { Gift } from '../gifts/gift.model';
+
 import { useContext, useState, useEffect } from 'react';
 import AppContext from '../context/AppContext';
 import GiftFC from '../components/GiftFC';
 import '../styles/GiftList.sass';
 import xmas from '../images/undraw-xmas-surprise.svg';
+
+const totalPriceReducer = (prev: number, curr: Gift): number => {
+  return prev + (curr.price * curr.quantity);
+}
 
 const GiftList: React.FC = () => {
   const { state, removeAll }: ContextProps = useContext(AppContext);
@@ -21,6 +27,9 @@ const GiftList: React.FC = () => {
               <GiftFC key={gift.id} gift={gift} />
             ))}
           </ul>
+          <div className='total-container'>
+            <p>Total: $ {(state?.gifts.reduce(totalPriceReducer, 0)!).toFixed(2)}</p>
+          </div>
           <div className='noGifts-container'>
             <button onClick={removeAll} className='noGifts-btn'>Remove ALL</button>
           </div>
