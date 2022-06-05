86d584c2dfa62b2624c575a25de65e87c95bdbfb
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-05 02:19:14 -0300

    [FEAT]: useInitialState hook created. TypeScript added.

---
+++ src/hooks/useInitialState.ts
@@ -0,0 +1,101 @@
+import { InitialState } from '../types';
+import { Gift } from '../gifts/gift.model';
+import { AddGiftDto, EditGiftDto } from '../gifts/gift.dto';
+
+import { useState, useEffect } from 'react';
+
+const initialState: InitialState = {
+  gifts: JSON.parse(localStorage.getItem('gifts')!) || [],
+  lastId: JSON.parse(localStorage.getItem('lastId')!) || 1
+}
+
+const useInitialState = () => {
+  const [state, setState] = useState(initialState);
+
+  useEffect(() => {
+    localStorage.setItem('gifts', JSON.stringify(state.gifts));
+    localStorage.setItem('lastId', JSON.stringify(state.lastId));
+  }, [state.gifts, state.lastId])
+
+
+  const addGift = (payload: AddGiftDto): string => {
+    const newGift: Gift = {
+      id: state.lastId,
+      gift: payload.gift,
+      price: payload.price,
+      to: payload.to,
+      image: payload.image,
+      quantity: payload.quantity
+    }
+
+    setState({
+      ...state,
+      gifts: [...state.gifts, newGift],
+      lastId: state.lastId + 1
+    })
+
+    return `${newGift.gift} #${newGift.id} created successfully`;
+  }
+
+
+  const getGift = (id: Gift['id']): number | undefined => {
+    const index = state.gifts.findIndex(gift => gift.id === id);
+
+    return index !== -1 ? index : undefined;
+  }
+
+
+  const editGift = (id: Gift['id'], payload: EditGiftDto): string => {
+    const index = getGift(id);
+
+    if (!index) return `Gift with ID #${id} not found`;
+
+    const prevData = state.gifts[index];
+    const newGift = {...prevData, ...payload};
+
+    state.gifts.splice(index, 1, newGift);
+
+    setState({
+      ...state,
+      gifts: [...state.gifts]
+    })
+
+    return `Gift with ID #${id} edited successfully`;
+  }
+
+
+  const deleteGift = (id: Gift['id']): string => {
+    const index = getGift(id);
+
+    if (!index) return `Gift with ID #${id} deleted successfully`;
+
+    state.gifts.splice(index, 1);
+
+    setState({
+      ...state,
+      gifts: [...state.gifts]
+    })
+
+    return `Gift with ID #${id} deleted successfully`;
+  }
+
+
+  const deleteAll = (): string => {
+    setState({
+      gifts: [],
+      lastId: 1
+    })
+
+    return `All gifts deleted successfully`;
+  }
+
+  return {
+    state,
+    addGift,
+    editGift,
+    deleteGift,
+    deleteAll
+  }
+}
+
+export default useInitialState;
