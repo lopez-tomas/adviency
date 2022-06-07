4b51eb669613b0fa408449800aa90219e73d3f08
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-07 15:46:46 -0300

    [FEAT]: Adviency - Day 23 finished. Snowfall added.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { useState, useEffect } from 'react';
 import AppContext from './context/AppContext';
 import useInitialState from './hooks/useInitialState';
 import { Modal, Form, GiftList } from './containers/main';
+import Snowfall from 'react-snowfall';
 import './styles/App.sass';
 
 import volume from "./images/volume.svg";
 import mute from "./images/volume-mute.svg"
 
-const App = () => {
+interface Props {
+  children?: React.ReactNode;
+}
+
+const App: React.FC<Props> = ({ children }) => {
   const initialState = useInitialState();
   const [showAdd, setShowAdd] = useState(false);
   const [music, setMusic] = useState<HTMLAudioElement | null>(null);
@@ -16,10 +21,10 @@ const App = () => {
   useEffect(() => {
     const url = 'https://andresguanov.github.io/assets/christmas.mp3';
     const music = new Audio(url);
+    music.autoplay = true;
     music.volume = 0.12;
     music.muted = true;
     music.loop = true;
-    music.autoplay = true;
 
     setMusic(music);
     setIsMuted(music.muted);
@@ -36,37 +41,43 @@ const App = () => {
   }
 
   return (
-    <AppContext.Provider value={initialState}>
-      <main className="App">
-        <section className="Form-container">
-          <div>
-            <h1>Gifts</h1>
+    <>
+      <Snowfall />
 
-            <figure
-              onClick={handleMusic}
-              className="btn music-btn">
-              {isMuted
-                ? <img src={mute} alt="Music muted" />
-                : <img src={volume} alt="Music unmuted" />
-              }
-            </figure>
-          </div>
-          <button
-            onClick={() => setShowAdd(!showAdd)}
-            className="btn add-btn"
-          >
-            Add gift
-          </button>
+      <AppContext.Provider value={initialState}>
+        <main className="App">
+          <section className="Form-container">
+            <div>
+              <h1>Gifts</h1>
+
+              <figure
+                onClick={handleMusic}
+                className="btn music-btn">
+                {isMuted
+                  ? <img src={mute} alt="Music muted" />
+                  : <img src={volume} alt="Music unmuted" />
+                }
+              </figure>
+            </div>
+
+            <button
+              onClick={() => setShowAdd(!showAdd)}
+              className="btn add-btn"
+            >
+              Add gift
+            </button>
+          </section>
 
           <Modal show={showAdd} onClose={() => setShowAdd(false)}>
             <Form onClose={() => setShowAdd(false)} />
           </Modal>
-        </section>
-        <section className="GiftList-container">
-          <GiftList />
-        </section>
-      </main>
-    </AppContext.Provider>
+
+          <section className="GiftList-container">
+            <GiftList />
+          </section>
+        </main>
+      </AppContext.Provider>
+    </>
   )
 }
 
