a428c4faebc0ed226e0d79d6008bd9d6ce2a9666
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-07 00:25:02 -0300

    [FEAT]: using all.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
+import AppContext from './context/AppContext';
+import useInitialState from './hooks/useInitialState';
+import { Modal, Form, GiftList } from './containers/main';
+import './styles/App.sass';
 
 const App = () => {
+  const initialState = useInitialState();
+  const [showAdd, setShowAdd] = useState(false);
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <AppContext.Provider value={initialState}>
+      <main className="App">
+        <section className="Form-container">
+          <h1>Gifts</h1>
+          <button
+            onClick={() => setShowAdd(!showAdd)}
+            className="btn add-btn"
+          >
+            Add gift
+          </button>
+
+          <Modal show={showAdd} onClose={() => setShowAdd(false)}>
+            <Form onClose={() => setShowAdd(false)} />
+          </Modal>
+        </section>
+        <section className="GiftList-container">
+          <GiftList />
+        </section>
+      </main>
+    </AppContext.Provider>
   )
 }
 
