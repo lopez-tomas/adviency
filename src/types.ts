333c739e51b8e8b5420635835d4a7dbb3dba5053
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-07 00:23:02 -0300

    [FEAT]: InitalState, ContextProps & FormProps interfaces created. main.ts created to manage them all.

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+import { AddGiftDto, EditGiftDto } from './gifts/gift.dto';
+import { Gift } from './gifts/gift.model';
+
+export interface InitialState {
+  gifts: Gift[];
+  lastId: number;
+}
+
+export interface ContextProps {
+  state?: InitialState;
+  addGift?: (payload: AddGiftDto) => string;
+  editGift?: (id: Gift['id'], payload: EditGiftDto) => string;
+  deleteGift?: (id: Gift['id']) => string;
+  deleteAll?: () => void;
+}
+
+export interface FormProps {
+  onClose: () => void;
+  idGift?: Gift['id'];
+  duplicate?: boolean;
+}
