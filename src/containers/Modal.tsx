e16710f8cbe5a80a6b52eb3571dbb9b026eeb6ae
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-07 00:23:28 -0300

    [FEAT]: Modal, Form & GiftList containers created. TypeScript added.

---
+++ src/containers/Modal.tsx
@@ -0,0 +1,41 @@
+import { FormProps } from '../types';
+
+import { useEffect } from 'react';
+import '../styles/Modal.sass';
+
+interface Props extends Omit<FormProps, 'idGift' | 'duplicate'> {
+  show: boolean;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<Props> = ({ show, onClose, children }) => {
+  const closeOnEscapeKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      onClose();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', closeOnEscapeKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', closeOnEscapeKeyDown);
+    }
+  })
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="Modal" onClick={onClose}>
+      <div className="Modal-content" onClick={e => e.stopPropagation()}>
+        <main className="Modal-body">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default Modal;
---
+++ src/containers/main.ts
@@ -0,0 +1,9 @@
+import Modal from './Modal';
+import Form from './Form';
+import GiftList from './GiftList';
+
+export {
+  Modal,
+  Form,
+  GiftList
+}
